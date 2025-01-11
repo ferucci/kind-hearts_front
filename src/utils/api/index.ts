b985d98451d9c9cd6aab4e94b9ea9f8bb51ba247
Ferucci <development.first1@gmail.com>
2025-01-11 17:36:17 +0300

    delete logs and fixed loading

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -6,16 +6,15 @@ interface ResponseData<T> {
 export type GetResponseResult<T> = ResponseData<T> | null;
 
 const checkResponse = async (res: Response) => {
-  console.log(res)
   if (res.ok || res.status === 201) {
     return res.json();
   }
   const err = await res.json();
+  console.log('error: ', err);
   return await Promise.reject(err);
 };
 
 export const getResponse = async <T>(url: string): Promise<GetResponseResult<T>> => {
-  console.log(url)
   const res = await fetch(url, {
     method: "GET",
     headers: {
